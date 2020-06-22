4f715f892545153cf61dbf8e203ec80f88662734
tnzk <tnzk.marge@gmail.com>
2020-06-23 00:59:34 +0900

    A bot prototype just echoing

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # covid19bot
+
+
+```
+$ yarn install
+$ yarn dev
+$ ngrok http -config ngrok.yml 3000
+```
+
+(Run ngrok in another terminal)
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,51 @@
-function hello(name: string): string {
-  return `Hello, ${name}!`;
+import {
+  // main APIs
+  Client,
+  middleware,
+
+  // exceptions
+  JSONParseError,
+  SignatureValidationFailed,
+
+  // types
+  TemplateMessage,
+  WebhookEvent,
+} from "@line/bot-sdk";
+import Express from 'express';
+import dotenv from "dotenv";
+
+dotenv.config();
+
+let safe = (expr: string | undefined) => (expr ? expr : '');
+const config = {
+  channelAccessToken: safe(process.env.MESSAGE_API_CHANNEL_ACCESS_TOKEN),
+  channelSecret: safe(process.env.MESSAGE_API_CHANNEL_SECRET)
+};
+
+const client = new Client(config);
+function handleEvent(event: WebhookEvent) {
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    return Promise.resolve(null);
+  }
+
+  client.replyMessage(event.replyToken, {
+    type: 'text',
+    text: event.message.text
+  }).catch(e => {
+    console.log(event);
+    console.log(e.originalError.response.data);
+  });
+
+  return Promise.resolve(null);
 }
 
-console.log(hello("World"));
+const app = Express();
+app.post('/webhook', middleware(config), (req, res) => {
+  Promise
+    .all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result))
+});
+
+console.log('Starting the server...');
+
+app.listen(3000);
